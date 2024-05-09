026100c83709942ad19ce7f7971d40968200c23c
JuanSpinozza <espinasavargasjuandiego@gmail.com>
2024-05-08 23:18:19 -0500

    Creación de modelo de reservas para la base de datos

---
+++ src/models/reservasSchema.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const reservasSchema = mongoose.Schema({
+    nombreCliente: {
+        type: String,
+        required: true
+    },
+    numeroPersonas: {
+        type: Number,
+        required: true
+    },
+    mesa: {
+        type: Number,
+        required: true
+    },
+    fecha: {
+        type: Date,
+        required: true
+    },
+    hora: {
+        type: String,
+        required: true
+    },
+    estado: {
+        type: String,
+        enum: ['Pendiente', 'Confirmada', 'Cancelada'],
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Reservas', reservasSchema);
