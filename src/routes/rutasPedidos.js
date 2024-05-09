44814047aa404fee1a6bf1b2fb03e037a62596e3
JuanSpinozza <espinasavargasjuandiego@gmail.com>
2024-05-08 23:57:45 -0500

    Cambios en el archivo rutasPedidos.js
    Se cambió el nombre de routes a router, se corrigió el nombre del router en cada método y se agregó la respectiva línea para exportar el router.

--- src/routes/rutasPedidos.js
+++ src/routes/rutasPedidos.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const routes = express.Router();
+const router = express.Router();
 const pedidos = require('../models/pedidosSchema');
 //Se pone el token de validacion para que cualquiera que no este con la sesion iniciada no pueda entrar y pedir platos
 const verifyToken = require('./tokenValidacion');
+const { default: mongoose } = require('mongoose');
 
-routes.get('/carrito', verifyToken, (req, res) => {
+router.get('/carrito', verifyToken, (req, res) => {
     pedidos.find()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }))
@@ -17,14 +18,14 @@ router.get("/carrito/:id", (req, res) => {
         .catch((error) => res.json({message: error}));
 });
 
-routes.post('/carrito', verifyToken, (req, res) => {
+router.post('/carrito', verifyToken, (req, res) => {
     let pedido = pedidos(req.body)
     pedido.save()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }))
 })
 
-routes.put('/carrito/estado/:id', verifyToken, (req, res) => {
+router.put('/carrito/estado/:id', verifyToken, (req, res) => {
     let id = req.params.id
     let estado = req.body;
     pedidos.update({ _id: id }, { $set: { estado } })
@@ -38,3 +39,5 @@ router.delete("/carrito/:id", verifyToken, (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.json({message: error}));
 });
+
+module.exports = router;
