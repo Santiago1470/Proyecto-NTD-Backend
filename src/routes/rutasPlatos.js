4a08ab538057814ca5752a324cb8a69425a7fe8c
JuanSpinozza <143860722+JuanSpinozza@users.noreply.github.com>
2024-05-10 12:50:01 -0500

    Validación de si el usuario es administrador para los métodos HTTP de platos

--- src/routes/rutasPlatos.js
+++ src/routes/rutasPlatos.js
@@ -2,41 +2,42 @@ const express = require('express');
 const router = express.Router();
 const platos = require('../models/platosSchema');
 const verifyToken = require('./tokenValidacion');
+const admin = require('./administrador')
 
-router.post("/platos", verifyToken, (req, res) => {
+router.post("/platos", verifyToken, admin, (req, res) => {
     let plato = platosSchema(req.body);
     plato.save()
         .then((data) => res.json(data))
-        .catch((error) => res.json({message: error}));
+        .catch((error) => res.json({ message: error }));
 });
 
 router.get("/platos", (req, res) => {
     platos.find()
         .then((data) => res.json(data))
-        .catch((error) => res.json({message: error}));
+        .catch((error) => res.json({ message: error }));
 });
 
 router.get("/platos/:id", (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     platos.findById(id)
         .then((data) => res.json(data))
-        .catch((error) => res.json({message: error}));
+        .catch((error) => res.json({ message: error }));
 });
 
-router.put("/platos/:id", verifyToken, (req, res) => {
-    const {id} = req.params;
-    const {nombre, descripcion, precio, categoria, ingredientes, imagen} = req.body;
-    platos.updateOne({_id: id}, {
-        $set: {nombre, descripcion, precio, categoria, ingredientes, imagen}
+router.put("/platos/:id", verifyToken, admin, (req, res) => {
+    const { id } = req.params;
+    const { nombre, descripcion, precio, categoria, ingredientes, imagen } = req.body;
+    platos.updateOne({ _id: id }, {
+        $set: { nombre, descripcion, precio, categoria, ingredientes, imagen }
     }).then((data) => res.json(data))
-        .catch((data) => res.json({message: error}));
+        .catch((data) => res.json({ message: error }));
 });
 
-router.delete("/platos/:id", verifyToken, (req, res) => {
-    const {id} = req.params;
+router.delete("/platos/:id", verifyToken, admin, (req, res) => {
+    const { id } = req.params;
     platos.findByIdAndDelete(id)
         .then((data) => res.json(data))
-        .catch((error) => res.json({message: error}));
+        .catch((error) => res.json({ message: error }));
 });
 
 module.exports = router;
