a93a78e649e9ea9d1623672bf8cab1aa59423dea
JuanSpinozza <143860722+JuanSpinozza@users.noreply.github.com>
2024-05-10 12:46:06 -0500

    Creación de administrador para poder hacer uso de las rutas CRUD platos

--- src/routes/tokenValidacion.js
+++ src/routes/tokenValidacion.js
@@ -1,13 +1,17 @@
 const jwt = require('jsonwebtoken');
-const verifyToken = (req, res, next) => {
-    const token = req.header('access-token')
-    if (!token) return res.status(401).json({ error: '¡Lo sentimos!, debes iniciar sesion para realizar la opción.' })
+const user = require('../models/usuarioSchema');
+const verifyToken = async (req, res, next) => {
+    const token = req.header('access-token');
+    if (!token) return res.status(401).json({ error: '¡Lo sentimos!, debes iniciar sesión para realizar la opción.' });
     try {
-        const verified = jwt.verify(token, process.env.SECRET)
-        req.userId = verified.id;
-        next() 
+        const verified = jwt.verify(token, process.env.SECRET);
+        const usuario = await user.findById(req.userId)
+            .then((data) => res.json(data))
+            .catch((error) => res.json({ message: error }));
+        req.usuario = usuario;
+        next();
     } catch (error) {
-        res.status(400).json({ error: 'El token no es válido' })
+        res.status(400).json({ error: 'El token no es válido' });
     }
 }
 module.exports = verifyToken;
