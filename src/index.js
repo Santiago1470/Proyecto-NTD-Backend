4ed4374978f419843c2972e3d0f06c2c3d04191a
Santiago1470 <st147086@gmail.com>
2024-05-09 22:51:52 -0500

    Se agregó el router correspondiente a los platos
    En el archivo index.js se importó el router rutasPlatos y se agregó la respectiva gestión de la ruta usando el middelware.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ const PORT = 3000;
 const parser = require("body-parser");
 const mongoose = require("mongoose");
 // Routers
+const platosRoutes = require("./routes/rutasPlatos");
 
-// const mongoose = require("mongoose");
 require("dotenv").config();
 app.use(parser.urlencoded({extended: false}));  // permite leer los datos que vienen en la petición
 app.use(parser.json()); // transforma los datos a formato JSON
 // Gestión de las rutas usando el middleware
-
+app.use("/api", platosRoutes);
 // Conexión a base de datos
 // mongoose
 //     .connect(process.env.MONGODB_URI)
